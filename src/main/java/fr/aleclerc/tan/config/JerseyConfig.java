8d24c90fbb988559e3407a7b5015fb370502c431
Amaury LECLERC <amauryleclerc@hotmail.fr>
2017-11-26 19:03:06 +0100

    correction config jersey

--- src/main/java/fr/aleclerc/tan/config/JerseyConfig.java
+++ src/main/java/fr/aleclerc/tan/config/JerseyConfig.java
@@ -1,16 +1,31 @@
 package fr.aleclerc.tan.config;
 
-import javax.ws.rs.ApplicationPath;
-
 import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.Path;
+import java.util.Map;
 
 @Configuration
 @ApplicationPath("/api")
 public class JerseyConfig extends ResourceConfig {
-		
-	public JerseyConfig() {
-		packages("fr.aleclerc.tan.resources");
-		packages("fr.aleclerc.tan.filters");
-	}
+
+    @Autowired
+    ApplicationContext appCtx;
+
+    public JerseyConfig() {
+    }
+
+    @PostConstruct
+    public void setup() {
+        Map<String, Object> beans = appCtx.getBeansWithAnnotation(Path.class);
+        for (Object o : beans.values()) {
+            register(o);
+        }
+    }
+
 }
